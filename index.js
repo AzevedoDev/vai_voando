9e2569ed8aa2f536b559addcc55018c06b6d24e7
Pedro Azevedo <theazevedo@outlook.com>
2018-11-04 02:12:04 -0200

    email

--- index.js
+++ index.js
@@ -23,3 +23,32 @@ app.listen(port, (err) => {
         console.log('Umbler listening on port %s', port);
     }
 })
+
+// const nodeMaieler = require('nodemailer')
+
+// const trasnporter = nodeMaieler.createTransport({
+//     host: 'smtp.umbler.com',
+//     port: 587,
+//     secure: false,
+//     auth:{
+//         user: 'no-reaply@vaivoandoexpress.com.br',
+//         pass: 'Pa22101350'
+//     },
+//     tls: {rejectUnauthorized: false}
+// })
+
+// const mailOpitions = {
+//     from: 'no-reaply@vaivoandoexpress.com.br',
+//     to: 'pazevedo2210@gmail.com',
+//     text: 'bem facil, sera?'
+// }
+
+// trasnporter.sendMail(mailOpitions, (error,info)=>{
+//     if (error) {
+//         console.log(error);
+        
+//     } else {
+//         console.log('Email enviado: ' + info.response);
+        
+//     }
+// })
